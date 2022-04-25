972880ef33857de6acb25cb33e8031d356f2b328
Cheroki84 <miguelangelmorato84@gmail.com>
2022-04-26 00:33:20 +0200

    connection to firebabe firestore made using environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
 import Header from './components/header/Header'
+import db from './utils/firebase/firebaseConfig'
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDocs(collection(db, 'usuarios'))
+      data.forEach((doc) => {
+        console.log(doc.data())
+      })
+    }
+    getData()
+  }, [])
 
   return (
     <div>
