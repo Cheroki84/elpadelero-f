13a5dd8fb336e1d2d24695e1dd20ea8f66fbf3d5
Cheroki84 <miguelangelmorato84@gmail.com>
2022-04-27 23:31:30 +0200

    deploy of project with firebase hosting

--- src/utils/firebase/firebaseConfig.ts
+++ src/utils/firebase/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+  apiKey: "AIzaSyAAJ62EkA6dPF1EtmJzQR8dnQ4xj2Y34i4",
+  authDomain: "elpadelero-24e60.firebaseapp.com",
+  projectId: "elpadelero-24e60",
+  storageBucket: "elpadelero-24e60.appspot.com",
+  messagingSenderId: "689191120604",
+  appId: "1:689191120604:web:c8bcb32851193f17cd0e19",
+  measurementId: "G-H5R9594SR2"
 };
 
 // Initialize Firebase
